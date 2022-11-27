e6dedc1158cace4581c1ea645362de77fd22aa74
Nethelbert Blasse <12989615+Gilblasse@users.noreply.github.com>
2022-11-27 05:41:12 +0000

    init scraper

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-test
+const puppeteer = require("puppeteer");
+
+async function WebScrapper(){
+	const browser = await puppeteer.launch();
+    const browserPage = await browser.newPage();
+    await browserPage.goto('https://html.com/resources/free-html-templates/');	
+
+    const page = await browserPage.evaluate(resultsSelector => {
+        console.log({ resultsSelector })
+        return [...document.querySelectorAll(resultsSelector)]
+    })
+
+	console.log({ page })
+	await browser.close();
+}
+
+WebScrapper()
